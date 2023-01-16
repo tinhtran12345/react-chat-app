b0ec8a0072b3b6beac59c6d6cc225d40f8712e2e
tinhtran12345 <tinhtran04012002@gmail.com>
2023-01-17 00:03:39 +0700

    init chat-app-v1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// import './App.css';
+import "./dist/styles.css"
+// import { Home } from "./Pages/Home";
+// import { Login } from "./Pages/Login";
+import { Register } from "./Pages/Register";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home/> */}
+      <Register/>
+      {/* <Login/> */}
+    </div>
+  );
+}
+
+export default App;
